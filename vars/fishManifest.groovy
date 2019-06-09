f6061190d8d6bd35912a2257495edff65dbfe0c9
Alex Tu <alex.tu@canonical.com>
2019-06-09 22:38:37 +0800

    init a branch for library
    refer to https://jenkins.io/doc/book/pipeline/shared-libraries/

---
+++ vars/fishManifest.groovy
@@ -0,0 +1,27 @@
+//def call(String project = 'somerville', String series, String target, String base, String update,String delete ) {
+def call(Map config) {
+    node {
+        env.base = "${config.base}"
+        env.project = "somerville"
+        env.target = "${config.target}"
+        env.series = "${config.series}"
+        env.update = "${config.update}"
+        env.delete = "${config.delete}"
+        script {
+            env.DOCKER_REPO=globalVar.internalDockerRepo()
+            env.DOCKER_VOL=globalVar.internalDockerVolum()
+            try {
+            sh '''#!/bin/bash
+                set -ex
+                RUN_DOCKER_TAIPEI_BOT="docker run --name oem-taipei-bot-${BUILD_TAG}-${STAGE_NAME} --rm -h oem-taipei-bot --volumes-from ${DOCKER_VOL} ${DOCKER_REPO}/oem-taipei-bot"
+                command="fish-manifest -p ${project} -r ${series} -e -c --target ${target} ${base} --postRTS "
+                [ -n "${update}" ] && command="$command -u ${update}"
+                [ -n "${delete}" ] && command="$command --delete ${delete}"
+                $RUN_DOCKER_TAIPEI_BOT $command
+            '''
+            } catch (e) {
+                error("exception:" + e)
+            }
+        }
+    }
+}
---
+++ vars/globalVar.groovy
@@ -0,0 +1,4 @@
+def internalDockerRepo() { return "somerville-jenkins.cctu.space:5000" }
+def internalDockerVolum() { return "docker-volumes" }
+
+
