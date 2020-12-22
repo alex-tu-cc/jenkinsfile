89dd147ca0366f490ffc276f5e849a72302cd4b1
Alex Tu <alex.tu@canonical.com>
2020-12-23 01:51:44 +0800

    fishmanifest for master

--- vars/fishManifest.groovy
+++ vars/fishManifest.groovy
@@ -14,10 +14,19 @@ def call(Map config) {
             sh '''#!/bin/bash
                 set -ex
                 RUN_DOCKER_TAIPEI_BOT="docker run --name oem-taipei-bot-${BUILD_TAG}-${STAGE_NAME} --rm -h oem-taipei-bot --volumes-from ${DOCKER_VOL} ${DOCKER_REPO}/oem-taipei-bot"
-                if [ "${series}" == "bionic" ]; then
-                    command="fish-manifest -b -p ${project} -r ${series} -e -c --target ${target} ${base} --postRTS "
+                if [ -n "${base}" ] && [ "${base}" != "null" ]; then
+                    if [ "${series}" == "bionic" ]; then
+                        command="fish-manifest -b -p ${project} -r ${series} -e -c --target ${target} ${base} --postRTS "
+                    else
+                        command="fish-manifest --git -b -p ${project} -r ${series} -e -c --target ${target} ${base} --postRTS "
+                    fi
                 else
-                    command="fish-manifest --git -b -p ${project} -r ${series} -e -c --target ${target} ${base} --postRTS "
+                    # reflash master manifest
+                    if [ "${series}" == "bionic" ]; then
+                        command="fish-manifest -b -p ${project} -r ${series} -e -c --target ${target} ${target}"
+                    else
+                        command="fish-manifest --git -b -p ${project} -r ${series} -e -c --target ${target} ${target}"
+                    fi
                 fi
                 [ -n "${update}" ] && [ "${update}" != "null" ] && command="$command -u ${update}"
                 [ -n "${delete}" ] && [ "${delete}" != "null" ] && command="$command --delete ${delete}"
